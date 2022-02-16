6a6bfcd227ec0a4227598768c19ea422d8e99cbc
abel@abellee.cn <Abel159460>
2022-02-16 16:06:36 +0800

    second day

---
+++ 03_using_namespace.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+    return 0;
+}
+
