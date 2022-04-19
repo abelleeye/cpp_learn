ee8b2308b9e3c56e480077e249e51c8d3962a6b8
abel@abellee.cn <Abel159460>
2022-04-19 22:01:39 +0800

    update

---
+++ chapter_1-4/35_structur.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+struct inflatable
+{
+    char name[20];
+    float volume;
+    double price;
+};
+
+int main() {
+    using namespace std;
+    inflatable guest =
+            {
+            "Glorious Gloria",
+            1.88,
+            29.99
+            };
+    inflatable pal =
+            {
+            "Audacious Arthur",
+            3.12,
+            32.99
+            };
+    // NOTE: some implementations require using
+    // static inflatable guest =
+
+    cout << "Expand your guest list with " << guest.name;
+    cout << " and " << pal.name << "!\n";
+
+    // pal.name is the name member of the pal variable
+    cout << "You can have both for $";
+    cout << guest.price + pal.price << "!\n";
+    return 0;
+}
+
